bb86380be1971dc05d435234ae7f98278c942d08
翻墙老铁 <99132134+fqmama@users.noreply.github.com>
2022-02-07 21:04:10 +0800

    Update task.sh

--- files/task.sh
+++ files/task.sh
@@ -1,1 +1,27 @@
+PRG="$0"
+while [ -h "$PRG" ] ; do
+  ls=`ls -ld "$PRG"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    PRG="$link"
+  else
+    PRG=`dirname "$PRG"`/"$link"
+  fi
+done
+PRGDIR=`dirname "$PRG"`
 #
+
+_classpath="."
+if [ `uname -a | grep -i -c cygwin` -ne 0 ]; then # Cygwin, translate the path
+    for k in "$PRGDIR"/lib/*.jar
+    do
+        _classpath="${_classpath};`cygpath -w ${k}`"
+    done
+else
+    for k in "$PRGDIR"/lib/*.jar
+    do
+        _classpath="${_classpath}:${k}"
+    done
+fi
+
+java -Xms512m -Xmx1024m -classpath "${_classpath}" "$@"
