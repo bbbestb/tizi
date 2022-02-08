4809fdff43a9b5ab2464cdc141414ea5fced6ecd
翻墙妈妈 <99132134+fqmama@users.noreply.github.com>
2022-02-08 20:15:56 +0800

    Create std.sh

---
+++ files/std.sh
@@ -0,0 +1,15 @@
+PRG="$0"
+while [ -h "$PRG" ] ; do
+  ls=`ls -ld "$PRG"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    PRG="$link"
+  else
+    PRG=`dirname "$PRG"`/"$link"
+  fi
+done
+PRGDIR=`dirname "$PRG"`
+#
+
+# call d2j_invoke.sh to setup java environment
+"$PRGDIR/d2j_invoke.sh" "com.googlecode.dex2jar.tools.StdApkCmd" "$@"
